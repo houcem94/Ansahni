11fdc8f33edbd8dfd0635c0c292289ecfe304c52
HOUIJI Houcem <houcem-externe.houiji@edf.fr>
2026-08-03 13:42:49 +0200

    update icons

--- web/sw.js
+++ web/sw.js
@@ -56,8 +56,8 @@ self.addEventListener('push', (event) => {
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: '/icons/icon-192.png',
-      badge: '/icons/icon-192.png',
+      icon: '/icons/icon-193.png',
+      badge: '/icons/icon-193.png',
       dir: 'rtl',
       lang: 'ar',
     })
